4a9bbf9ae96c63455b400eb439c15c5d5a5d8b29
yl7690 <yl7690@nyu.edu>
2021-03-30 05:51:57 -0400

    finished changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var menuButtonElem;
-var yinElem;
-var yangElem;
-var naviElem;
-var menuOpen = false;
-var menuClosed = true;
+// var menuButtonElem;
+// var yinElem;
+// var yangElem;
+// var naviElem;
+// var menuOpen = false;
+// var menuClosed = true;
 var A, B, C, D, E, F, G, H, I, J;
 var nameA, nameB, nameC, nameD, nameE, nameF, nameG, nameH, nameI, nameJ;
 var blank;
@@ -11,22 +11,22 @@ var largeStones, largeStone2, largeStone3, largeStone4;
 var jumpingStones;
 var i;
 
-window.addEventListener('DOMContentLoaded', (event) => {
-
-  console.log("loaded...");
-  console.log('open',menuOpen);
-  console.log(menuClosed);
-
-  // menuButtonElem = document.getElementsByClassName('menuButton')[0];
-  naviElem = document.getElementsByClassName('navigation')[0]
-  yinElem = document.getElementsByClassName('yinCover')[0];
-  yangElem = document.getElementsByClassName('yangCover')[0];
-
-
-  // menuButtonElem.addEventListener('click', openMenu);
-    yinElem.addEventListener('click', openMenu);
-    yangElem.addEventListener('click', openMenu);
-});
+// window.addEventListener('DOMContentLoaded', (event) => {
+//
+//   console.log("loaded...");
+//   console.log('open',menuOpen);
+//   console.log(menuClosed);
+//
+//   // menuButtonElem = document.getElementsByClassName('menuButton')[0];
+//   naviElem = document.getElementsByClassName('navigation')[0]
+//   yinElem = document.getElementsByClassName('yinCover')[0];
+//   yangElem = document.getElementsByClassName('yangCover')[0];
+//
+//
+//   // menuButtonElem.addEventListener('click', openMenu);
+//     yinElem.addEventListener('click', openMenu);
+//     yangElem.addEventListener('click', openMenu);
+// });
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
@@ -87,20 +87,20 @@ window.addEventListener('DOMContentLoaded', (event) => {
     J.addEventListener('mouseleave', hideNameJ);
 });
 
-function openMenu() {
-  console.log("opening menu...");
-  if (menuClosed) {
-    yinElem.classList.add('yinOpen');
-    naviElem.classList.add('showNavigation');
-    menuOpen = true;
-    menuClosed = false;
-  } else if (menuOpen) {
-    yinElem.classList.remove('yinOpen');
-    naviElem.classList.remove('showNavigation');
-    menuOpen = false;
-    menuClosed = true;
-  }
-}
+// function openMenu() {
+//   console.log("opening menu...");
+//   if (menuClosed) {
+//     yinElem.classList.add('yinOpen');
+//     naviElem.classList.add('showNavigation');
+//     menuOpen = true;
+//     menuClosed = false;
+//   } else if (menuOpen) {
+//     yinElem.classList.remove('yinOpen');
+//     naviElem.classList.remove('showNavigation');
+//     menuOpen = false;
+//     menuClosed = true;
+//   }
+// }
 
 function stopBouncing() {
   console.log('working')
